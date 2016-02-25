96b7d466c082acd61eac796469ea9f564f0a71a1
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2016-02-25 08:45:09 -0500

    Initial commit

---
+++ src/main/java/biblia/io/workflows/App.java
@@ -0,0 +1,13 @@
+package biblia.io.workflows;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
