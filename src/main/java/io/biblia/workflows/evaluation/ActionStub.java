5c81e20186e268ecbdc2e8db0620497a75bd4b21
Jadiel <jadielam@gmail.com>
2016-03-17 10:11:23 -0400

    Finsihed the action task that writes to HDFS files of a given size and takes a given time to compute.

--- src/main/java/io/biblia/workflows/evaluation/ActionStub.java
+++ src/main/java/io/biblia/workflows/evaluation/ActionStub.java
@@ -1,5 +1,16 @@
 package io.biblia.workflows.evaluation;
 
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
 import io.biblia.workflows.action.Action;
 import com.google.common.base.Preconditions;
 
@@ -19,7 +30,7 @@ import com.google.common.base.Preconditions;
  */
 public class ActionStub implements Action {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		if (args.length != 4) {
 			//Passing the wrong number of parameters.
@@ -60,19 +71,44 @@ public class ActionStub implements Action {
 	 * Writes a file to HDFS with the properties specified by the parameters.
 	 * @param filePath: The complete path to the file to write in the hadoop filesystem
 	 * @param size: The size of the file to produce in megabytes
+	 * @throws IOException 
 	 */ 
-	private void writeDataset(String filePath, int size) {
+	private void writeDataset(String filePath, int size) throws IOException {
+		Preconditions.checkNotNull(filePath);
+		if (size < 0) size = 0;
+		long totalBytes = size * 1000000L;
 		
 		//1. Write bogus data into that file until size in MBs is satisfied.
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(URI.create(filePath), conf);
-		OutputStream out = fs.create(new Path(dst));
-		IOUtils.copyBytes("testing", out, 4096, true);
+		OutputStream out = fs.create(new Path(filePath));
 		
+		long bytesCopied = 0;
+		while (bytesCopied < totalBytes) {
+			//Copy 400MB at a time
+			int bytesToCopy = (int)Math.min(400000000, Math.max(0, totalBytes - bytesCopied));
+			byte [] bytes = new byte[bytesToCopy];
+			InputStream is = new ByteArrayInputStream(bytes);
+			try{
+				IOUtils.copyBytes(is, out, 4096, false);
+				is.close();
+			}
+			catch(IOException e) {
+				is.close();
+				throw e;
+			}
+			bytesCopied += bytesToCopy;
+		}
+		
+		out.close();
 	}
 	
+	/**
+	 * This function will sleep for the amount of time in milliseconds given
+	 * @param milliseconds
+	 */
 	private void compute(long milliseconds) {
-		Preconditions.checkArgument(milliseconds > 0);
+		if (milliseconds < 0) milliseconds = 0;
 		try {
 			Thread.sleep(milliseconds);
 		}
