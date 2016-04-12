29a53287917cbf6381858b42bbd3e2c13ea48e98
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2016-04-12 11:15:08 -0400

    Fixing compilation errors

--- src/main/java/io/biblia/workflows/manager/action/MongoActionPersistance.java
+++ src/main/java/io/biblia/workflows/manager/action/MongoActionPersistance.java
@@ -1,28 +1,38 @@
 package io.biblia.workflows.manager.action;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import io.biblia.workflows.definition.Action;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MongoActionPersistance implements ActionPersistance {
     
-    public static final String ACTIONS_COLLECTION;
+    public static final String ACTIONS_COLLECTION = "actions_cl";
+    public static final String WORKFLOWS_DATABASE = "workflows_db";
     private final MongoClient mongo;
     private final MongoDatabase workflows;
-    private final MongoCollection actions
+    private final MongoCollection<Document> actions;
     
     public MongoActionPersistance(MongoClient mongo) {
-        this.client = mongo;
-        this.actions = this.mongo.getDatabase(WORKFLOWS_DATABASE);
-        
+        this.mongo = mongo;
+        this.workflows = this.mongo.getDatabase(WORKFLOWS_DATABASE);
+        this.actions = this.workflows.getCollection(ACTIONS_COLLECTION);
+        Map<String, String> map = new HashMap<String, String>();
+
     }
     
     @Override
     public List<Action> getAvailableActions() {
-      List<Action> toReturn = new ArrayList<Action> toReturn;
+    	List<Action> toReturn = new ArrayList<Action>();
       
-      return toReturn;
+    	return toReturn;
     }
     
     @Override
