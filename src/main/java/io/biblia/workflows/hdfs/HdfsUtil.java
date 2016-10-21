8df1b421b3193cb9769abdf487f091c7a67b0465
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2016-10-20 21:46:36 -0400

    Changed the way we delete files

--- src/main/java/io/biblia/workflows/hdfs/HdfsUtil.java
+++ src/main/java/io/biblia/workflows/hdfs/HdfsUtil.java
@@ -59,21 +59,13 @@ public class HdfsUtil implements ConfigurationKeys {
 	/**
 	 * Removes a path. 
 	 * If the path is a folder, it removes the folder and everything in it.
+	 * It does it recursively.
 	 * If it is a file, it removes the file.
 	 * @param path The path does not include the base url of the namenode.
 	 */
 	public static void deletePath(String path) throws IOException {
 		Preconditions.checkNotNull(path);
-		File dfsFile = new File(path);
-		if (dfsFile.exists()) {
-			
-			//If it is a directory removes the directory and everything in it.
-			//If it is a simple file, removes the file.
-			boolean deleted = FileUtil.fullyDelete(dfsFile);
-			if (!deleted) {
-				throw new IOException("Path "+ path + " could not be deleted");
-			}
-		}
+		fs.delete(new Path(path), true);
 	}
 	
 	public static void writeStringToFile(String text, String folderPath, String fileName) throws IllegalArgumentException, IOException {
